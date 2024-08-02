38cb32245d85b9612b8b4e55effc2cb5fc36e1bf
piero25xx <pierremhanna3@gmail.com>
2024-08-02 15:01:47 +0300

    task 4

---
+++ task_4.sql
@@ -0,0 +1,12 @@
+-- Query to get detailed information about the 'books' table
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Column Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_DEFAULT AS 'Default Value',
+    EXTRA AS 'Extra Information'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'Books';
+
